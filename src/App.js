06c590849e62ea9669b8bd20aa663e790c1aee6c
Jerome <jerome.pihet@viacesi.fr>
2022-03-04 11:31:28 +0100

    ADD : Navbab, Routing and Theme

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Header from './Components/Header';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App dark flex flex-col min-h-screen">
+          <Header/>
+          <div className='grow dark:bg-primary-300'>
+          <Routes>
+            <Route path="home" element={<></>} />
+            <Route path="*" element={<Navigate to="/home" />} />
+          </Routes>
+          </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#2B031C',
+        'primary-300': '#3D0528',
+        'primary-500': '#210215',
+        'secondary': '#EEE0CB',
+        'cta': '#EE6055',
+      }
+    },
+  },
+  plugins: [],
+}
